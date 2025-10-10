647d38ce2e9d1d1295f96add2acdfe28c1935630
hwang-hyesung <119783622+hwang-hyesung@users.noreply.github.com>
2025-10-10 19:06:14 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.73 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    int answer = 0;
+    
+    queue<pair<int, int>> q;
+    q.push({truck_weights[0], 1});
+    int bw = truck_weights[0], sec=1, num=1;
+   
+    while(!q.empty()){
+        sec++;
+        
+        if(sec >= q.front().second+bridge_length){
+            cout << q.front().first << " out of bridge at " <<  sec << "\n";
+            bw -= q.front().first;
+            q.pop();
+        }
+        if(num >= truck_weights.size() || bw+truck_weights[num]>weight) continue;
+        else {
+            cout << truck_weights[num] << " enter in bridge at " <<  sec << "\n";
+            q.push({truck_weights[num], sec});
+            bw += truck_weights[num];
+            num++;
+        }
+        
+    }
+    answer = sec;
+    return answer;
+}
