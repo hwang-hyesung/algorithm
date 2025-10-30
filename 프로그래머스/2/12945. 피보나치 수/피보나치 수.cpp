337978d7d8902ece67e1cae7427eb3d23ae58b2b
hwang hyesung <hwanghs7008@g.skku.edu>
2025-10-30 13:35:26 +0900

    [level 2] Title: 피보나치 수, Time: 0.55 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    vector<int> fibo(n+1);
+    fibo[0]=0;
+    fibo[1]=1;
+    for(int i=2;i<=n;i++){
+        fibo[i] = (fibo[i-1] + fibo[i-2]) % 1234567;
+    }
+    return fibo[n];
+}
