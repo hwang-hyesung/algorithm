bbf4108addd1985838de01d05ff7170a2fea373e
hwang hyesung <hwanghs7008@g.skku.edu>
2025-10-30 20:49:59 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n) {
+    long long answer = 0;
+    vector<int> v;
+    v.push_back(0);
+    v.push_back(1);
+    v.push_back(2);
+    for(int i=3;i<=n;i++){
+        v.push_back((v[i-1] + v[i-2])%1234567);
+    }
+    return v[n];
+}
