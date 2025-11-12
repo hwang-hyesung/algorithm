5451a5c23a22253e054e56bbc308fb9bfde7d060
hwang-hyesung <hwanghs7008@g.skku.edu>
2025-11-12 22:03:44 +0900

    [level 4] Title: 도둑질, Time: 3.40 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.cpp
+++ 프로그래머스/4/42897. 도둑질/도둑질.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 using namespace std;
 
-int robLinear(const vector<int>& money, int start, int end) {
+int rob(const vector<int>& money, int start, int end) {
     int prev = 0;
     int curr = 0;
     for (int i = start; i <= end; ++i) {
@@ -18,7 +18,7 @@ int solution(vector<int> money) {
     int n = money.size();
     if (n == 1) return money[0]; 
 
-    int case1 = robLinear(money, 0, n - 2);
-    int case2 = robLinear(money, 1, n - 1);
+    int case1 = rob(money, 0, n - 2);
+    int case2 = rob(money, 1, n - 1);
     return max(case1, case2);
 }
