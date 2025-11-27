4c2fa57e1d0ca0f8d977ac33f1cacbb8ee4402f6
hwang-hyesung <hwanghs7008@g.skku.edu>
2025-11-27 20:14:19 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    bool answer = true;
+    int sum = 0, num = x;
+    while(num>0){
+        sum += num%10;
+        num /=10;
+    }
+    return x%sum==0;
+}
