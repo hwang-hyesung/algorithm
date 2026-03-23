f8ea750e2e65787a19df9ef26fff98af8d67d5fb
hwang-hyesung <hwanghs7008@g.skku.edu>
2026-03-23 10:53:09 +0900

    기출 문제 / 2025 상반기 오전 1번 문제 / 민트 초코 우유 2

--- 코드트리/mintchocomilk.cpp
+++ 코드트리/mintchocomilk.cpp
@@ -1,228 +1,214 @@
-#include <iostream>
-#include <cstring>
-#include <cmath>
-#include <vector>
-#include <queue>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int n, t, power[51][51], dy[] = {-1, 1, 0, 0}, dx[] = {0, 0, -1, 1};
-string a[51][51], kind[] = {"T", "C", "M", "CM", "TM", "TC", "TCM"}; // 0 1 2 3 4 5 6
-int heart[7], junpa[51][51], visited[51][51];
-vector<pair<int, pair<int, int>>> king[3];
+int N, T, dy[] = {-1, 1, 0, 0}, dx[] = {0, 0, -1, 1};
+string types[55][55];
+int power[55][55];
+bool visited[55][55], spread[55][55];
+map<string, int> tmap;
 
-auto comp = [](const auto &a, const auto &b)
+struct Node
 {
-    if (a.first != b.first)
-        return a.first > b.first; // power 내림차순
-    if (a.second.second != b.second.second)
-        return a.second.second < b.second.second; // y 오름차순
-    return a.second.first < b.second.first;       // x 오름차순
+    string t;
+    int y, x, p;
+    bool operator<(const Node &other) const
+    {
+        if (t.length() != other.t.length())
+            return t.length() > other.t.length();
+        if (p != other.p)
+            return p < other.p;
+        if (y != other.y)
+            return y > other.y;
+        if (x != other.x)
+            return x > other.x;
+        return false;
+    }
 };
 
-void findKing(int y, int x)
+Node findTeam(int r, int c)
 {
-    if (visited[y][x] != 0)
-        return;
-    int mp = power[y][x], my = y, mx = x, count = 0;
-    string type = a[y][x];
+    Node n;
+    n.t = types[r][c];
+    n.y = r;
+    n.x = c;
+    n.p = power[r][c];
     queue<pair<int, int>> q;
-    q.push({y, x});
-    visited[y][x] = 1;
+    vector<pair<int, int>> v;
+    q.push({r, c});
+    visited[r][c] = 1;
+    power[r][c]--;
+    int cnt = 1;
     while (!q.empty())
     {
-        y = q.front().first;
-        x = q.front().second;
+        int y = q.front().first;
+        int x = q.front().second;
         q.pop();
-        count++;
-        for (int i = 0; i < 4; i++)
+
+        for (int d = 0; d < 4; d++)
         {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if (ny < 0 || nx < 0 || ny >= n || nx >= n || visited[ny][nx] != 0 || a[y][x] != a[ny][nx])
+            int ny = y + dy[d];
+            int nx = x + dx[d];
+            if (ny < 0 || ny >= N || nx < 0 || nx >= N || visited[ny][nx] != 0 || types[ny][nx] != n.t)
                 continue;
             q.push({ny, nx});
             visited[ny][nx] = 1;
-            if (mp < power[ny][nx])
+
+            if (power[ny][nx] > n.p)
             {
-                mp = power[ny][nx];
-                my = ny;
-                mx = nx;
+                n.y = ny;
+                n.x = nx;
+                n.p = power[ny][nx];
             }
-            else if (mp == power[ny][nx])
+            else if (power[ny][nx] == n.p)
             {
-                if (ny < my)
+                if (ny < n.y)
                 {
-                    my = ny;
-                    mx = nx;
+                    n.y = ny;
+                    n.x = nx;
+                    n.p = power[ny][nx];
                 }
-                else if (ny == my)
+                else if (ny == n.y)
                 {
-                    if (nx < mx)
+                    if (nx < n.x)
                     {
-                        my = ny;
-                        mx = nx;
+                        n.y = ny;
+                        n.x = nx;
+                        n.p = power[ny][nx];
                     }
                 }
             }
+
+            power[ny][nx]--;
+            cnt++;
         }
     }
-    power[my][mx] += count;
-    king[type.length() - 1].push_back({power[my][mx], {mx, my}});
+    power[n.y][n.x] += cnt;
+    n.p = power[n.y][n.x];
+    return n;
 }
 
-// 같은 음식끼리 그룹 지어서 대표자(파워크고 가장 좌상단)에게 1씩 줌
-void lunch()
+vector<int> count()
 {
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            findKing(i, j);
-        }
-    }
-}
+    vector<int> res(7, 0);
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            res[tmap[types[i][j]]] += power[i][j];
 
-// 대표자가 본인 파워%4 방향으로 본인 파워 -1 만큼 전파를 시도함
-//  전파가 더 세면 같아지고 전파자는 상대 신앙심+1만큼 깎임
-//  전차가 더 약하면 섞이고 전파자는 0 상대는 전파만큼 증가
-//  전파 된 상대는 당일에 전파는 못함 계속 당할순 있음
-void dinner()
-{
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < king[i].size(); j++)
-        {
-            int p = king[i][j].first - 1;
-            int y = king[i][j].second.second;
-            int x = king[i][j].second.first;
-            string type = a[y][x];
-            if (junpa[y][x] != 0)
-                continue;
-            // cout << type << " " << p << " " << y << " " << x <<"\n";
-            int ny = y, nx = x;
-            int d = (p + 1) % 4;
-            power[y][x] = 1;
-            while (true)
-            {
-                ny += dy[d];
-                nx += dx[d];
-                if (ny < 0 || nx < 0 || ny >= n || nx >= n || p <= 0)
-                    break;
-                if (a[ny][nx] == type)
-                    continue;
-                if (p > power[ny][nx])
-                {
-                    a[ny][nx] = type;
-                    power[ny][nx]++;
-                    junpa[ny][nx] = 1;
-                    p -= power[ny][nx];
-                    if (p <= 0)
-                        break;
-                }
-                else
-                {
-                    string ntype = type + a[ny][nx];
-                    string rtype = "";
-                    if (ntype.find("T") != string::npos)
-                        rtype += "T";
-                    if (ntype.find("C") != string::npos)
-                        rtype += "C";
-                    if (ntype.find("M") != string::npos)
-                        rtype += "M";
-                    a[ny][nx] = rtype;
-                    power[ny][nx] += p;
-                    junpa[ny][nx] = 1;
-                    break;
-                }
-            }
-            // for(int i=0;i<n;i++){
-            //     for(int j=0;j<n;j++){
-            //         cout << a[i][j] << " ";
-            //     }
-            //     cout << "\n";
-            // }
-            // for(int i=0;i<n;i++){
-            //     for(int j=0;j<n;j++){
-            //         cout << power[i][j]<< " ";
-            //     }
-            //     cout << "\n";
-            // }
-        }
-    }
-}
-
-void countHeart()
-{
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            for (int k = 0; k < 7; k++)
-            {
-                if (kind[k] == a[i][j])
-                    heart[k] += power[i][j];
-            }
-        }
-    }
+    return res;
 }
 
 int main()
 {
-    cin.tie(0)->sync_with_stdio(0);
-    cin >> n >> t;
-    for (int i = 0; i < n; i++)
+    cin >> N >> T;
+    for (int i = 0; i < N; i++)
     {
-        for (int j = 0; j < n; j++)
+        string str;
+        cin >> str;
+        for (int j = 0; j < N; j++)
         {
-            char aa;
-            cin >> aa;
-            a[i][j] = aa;
+            types[i][j] = str[j];
         }
     }
-    for (int i = 0; i < n; i++)
+
+    for (int i = 0; i < N; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < N; j++)
         {
             cin >> power[i][j];
         }
     }
 
-    memset(heart, 0, sizeof(heart));
-    while (t--)
+    tmap["CMT"] = 0;
+    tmap["CT"] = 1;
+    tmap["MT"] = 2;
+    tmap["CM"] = 3;
+    tmap["M"] = 4;
+    tmap["C"] = 5;
+    tmap["T"] = 6;
+
+    while (T--)
     {
-        memset(junpa, 0, sizeof(junpa));
         memset(visited, 0, sizeof(visited));
-        memset(heart, 0, sizeof(heart));
-        king[0].clear();
-        king[1].clear();
-        king[2].clear();
+        memset(spread, 0, sizeof(spread));
 
-        lunch();
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                power[i][j]++;
 
-        sort(king[0].begin(), king[0].end(), comp);
-        sort(king[1].begin(), king[1].end(), comp);
-        sort(king[2].begin(), king[2].end(), comp);
-
-        dinner();
-
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<n;j++){
-        //         cout << a[i][j] << " ";
-        //     }
-        //     cout << "\n";
-        // }
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<n;j++){
-        //         cout << power[i][j] << " ";
-        //     }
-        //     cout << "\n";
-        // }
-        countHeart();
-        for (int i = 6; i >= 0; i--)
+        priority_queue<Node> pq;
+        for (int i = 0; i < N; i++)
         {
-            cout << heart[i] << " ";
+            for (int j = 0; j < N; j++)
+            {
+                if (visited[i][j] == 0)
+                {
+                    pq.push(findTeam(i, j));
+                }
+            }
+        }
+
+        while (!pq.empty())
+        {
+            string t = pq.top().t;
+            int y = pq.top().y;
+            int x = pq.top().x;
+            int p = pq.top().p;
+            pq.pop();
+
+            if (spread[y][x] != 0)
+                continue;
+            int desire = p - 1;
+            int d = p % 4;
+            int sy = y, sx = x;
+            power[y][x] = 1;
+            if (desire == 0)
+                break;
+            while (true)
+            {
+                sy += dy[d];
+                sx += dx[d];
+                if (sy < 0 || sy >= N || sx < 0 || sx >= N)
+                    break;
+                if (types[sy][sx] == t)
+                    continue;
+                spread[sy][sx] = 1;
+                if (desire > power[sy][sx])
+                {
+                    types[sy][sx] = t;
+                    power[sy][sx]++;
+                    desire -= power[sy][sx];
+                }
+                else if (desire <= power[sy][sx])
+                {
+                    set<char> s;
+                    for (char c : t)
+                    {
+                        s.insert(c);
+                    }
+                    for (char c : types[sy][sx])
+                    {
+                        s.insert(c);
+                    }
+                    string nt = "";
+                    for (char c : s)
+                    {
+                        nt += c;
+                    }
+                    types[sy][sx] = nt;
+                    power[sy][sx] += desire;
+                    desire = 0;
+                }
+                if (desire <= 0)
+                    break;
+            }
+        }
+
+        vector<int> ans = count();
+        for (int a : ans)
+        {
+            cout << a << " ";
         }
         cout << "\n";
     }
+    return 0;
 }
